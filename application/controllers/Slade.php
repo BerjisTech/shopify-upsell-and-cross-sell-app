16b10df61bc35b01e5f9ea48abe2112a82452fe4
Berjis <berjistechnologies@gmail.com>
2020-11-08 16:23:06 +0300

    BAse URL

--- application/controllers/Slade.php
+++ application/controllers/Slade.php
@@ -239,10 +239,10 @@ class Slade extends CI_Controller
         $shop = $_GET['shop'];
         $api_key = $this->config->item('shopify_api_key');
         $scopes = "read_orders,write_orders,read_draft_orders,read_content,write_content,read_products,write_products,read_product_listings,read_customers,write_customers,read_inventory,write_inventory,read_locations,read_script_tags,write_script_tags,read_themes,write_themes,read_shipping,write_shipping,read_analytics,read_checkouts,write_checkouts,read_reports,write_reports,read_price_rules,write_price_rules,read_discounts,write_discounts,read_resource_feedbacks,write_resource_feedbacks,read_translations,write_translations,read_locales,write_locales";
-        $redirect_uri = base_url() . "generate_token";
+        $redirect_uri = "https://sleek-upsell.herokuapp.com/generate_token";
         
         // Build install/approval URL to redirect to
-        $install_url = "https://" . $shop . ".myshopify.com/admin/oauth/authorize?client_id=" . $api_key . "&scope=" . $scopes . "&redirect_uri=" . $redirect_uri;
+        $install_url = "https://" . $shop . ".myshopify.com/admin/oauth/authorize?client_id=" . $api_key . "&scope=" . $scopes . "&redirect_uri=" . urlencode($redirect_uri);
         // Redirect
         header("Location: " . $install_url);
         die();
