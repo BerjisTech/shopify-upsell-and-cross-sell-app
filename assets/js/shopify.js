d02f2fed7e5b93a7669e2b8f11ffc0d080bf1c9b
Berjis <berjistechnologies@gmail.com>
2020-12-05 15:01:49 +0300

    Setup Wizard

--- assets/js/shopify.js
+++ assets/js/shopify.js
@@ -970,7 +970,7 @@ jQuery(document).ready(function () {
         $('body').prepend('<style>.suw{display: table; width: 300px; height: 500px; background: #ffffff; position: absolute; bottom: 0px; left: 0px; z-index: 3000000;}.suw_head, .suw_footer{display: table; width: 100%; height: 50px !important; background: #981B1B !important; color: #ffffff;}.suw_body{overflow-Y: auto; display: table; width: 100%; height: 400px;}</style>');
         $('body').append('<div class="suw" id="suw">'+
         '<div class="suw_head">Setup Wizard</div>'+
-        '<div class="suw_body"></div>'+
+        '<div class="suw_body"><select><option>2</option><option>2</option><option>2</option><option>2</option></select></div>'+
         '<div class="suw_footer"></div>'+
         '</div>');
         dragElement(document.querySelector(".suw"));
