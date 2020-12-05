af33d0ba4e83bfdeb9de30d4702c6e56bd544931
Berjis <berjistechnologies@gmail.com>
2020-12-05 12:29:34 +0300

    Setup Wizard

--- assets/js/shopify.js
+++ assets/js/shopify.js
@@ -86,7 +86,8 @@ let s_s_w = g_s_s_w('https://sleek-upsell.herokuapp.com/s_s_w/' + Shopify.shop);
 jQuery(document).ready(function () {
 
     if (page_ss.includes(s_s_w)) {
-        alert('Setup Wizard');
+        sessionStorage.setItem('s_u_w', true);
+        createSUW();
     } else {
         console.log(page_ss);
         console.log(s_s_w);
@@ -963,6 +964,74 @@ jQuery(document).ready(function () {
             $('.sleek-price').css('font-size', settings['price_size']);
         }
 
+    }
+
+    function createSUW() {
+        $('body').prepend('<style>.suw{display: table; width: 300px; height: 500px; background: #ffffff;}</style>');
+        $('body').append('<div class="suw"></div>');
+        var dragItem = $('.suw');
+        var container = $('body');
+
+        var active = false;
+        var currentX;
+        var currentY;
+        var initialX;
+        var initialY;
+        var xOffset = 0;
+        var yOffset = 0;
+
+        container.addEventListener("touchstart", dragStart, false);
+        container.addEventListener("touchend", dragEnd, false);
+        container.addEventListener("touchmove", drag, false);
+
+        container.addEventListener("mousedown", dragStart, false);
+        container.addEventListener("mouseup", dragEnd, false);
+        container.addEventListener("mousemove", drag, false);
+
+        function dragStart(e) {
+            if (e.type === "touchstart") {
+                initialX = e.touches[0].clientX - xOffset;
+                initialY = e.touches[0].clientY - yOffset;
+            } else {
+                initialX = e.clientX - xOffset;
+                initialY = e.clientY - yOffset;
+            }
+
+            if (e.target === dragItem) {
+                active = true;
+            }
+        }
+
+        function dragEnd(e) {
+            initialX = currentX;
+            initialY = currentY;
+
+            active = false;
+        }
+
+        function drag(e) {
+            if (active) {
+
+                e.preventDefault();
+
+                if (e.type === "touchmove") {
+                    currentX = e.touches[0].clientX - initialX;
+                    currentY = e.touches[0].clientY - initialY;
+                } else {
+                    currentX = e.clientX - initialX;
+                    currentY = e.clientY - initialY;
+                }
+
+                xOffset = currentX;
+                yOffset = currentY;
+
+                setTranslate(currentX, currentY, dragItem);
+            }
+        }
+
+        function setTranslate(xPos, yPos, el) {
+            el.style.transform = "translate3d(" + xPos + "px, " + yPos + "px, 0)";
+        }
     }
 
 });
