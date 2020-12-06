df8703ec1bcb8a4fb8a56fb947ec112c17d61a6c
Berjis <berjistechnologies@gmail.com>
2020-12-06 10:34:47 +0300

    Setup Wizard

--- application/views/settings.php
+++ application/views/settings.php
@@ -65,7 +65,7 @@
                 </div>
 
             </div>
-            <div style="height: 100vh; background: #FAFAFA; text-align: center;">
+            <div style="height: 100vh; background: #FAFAFA; display: flex; align-items: center; justify-content: center;">
                 <div class="previewFrame" style="display: table; width: 100%; height: 100%;"></div>
                 <div style="display: flex; position: absolute; bottom: 10px; right: 10px; background: #fff; padding: 20px;">
                     <img src="https://cdn4.iconfinder.com/data/icons/48-bubbles/48/29.Mac-512.png" style="height: 50px; cursor: pointer;" onclick="$('.previewFrame').width('100%');$('.previewFrame').height('100%');" />
