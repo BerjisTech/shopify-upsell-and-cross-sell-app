2575a373b91b774ebc0ba24a1b63f5d1e02ae180
Berjis <berjistechnologies@gmail.com>
2020-12-10 16:54:45 +0300

    Helpscout

--- application/views/index.php
+++ application/views/index.php
@@ -113,26 +113,6 @@
         <div class="main-content">
             <?php include $page_name . '.php'; ?>
         </div>
-        <script type="text/javascript">
-            ! function(e, t, n) {
-                function a() {
-                    var e = t.getElementsByTagName("script")[0],
-                        n = t.createElement("script");
-                    n.type = "text/javascript", n.async = !0, n.src = "https://beacon-v2.helpscout.net", e.parentNode.insertBefore(n, e)
-                }
-                if (e.Beacon = n = function(t, n, a) {
-                        e.Beacon.readyQueue.push({
-                            method: t,
-                            options: n,
-                            data: a
-                        })
-                    }, n.readyQueue = [], "complete" === t.readyState) return a();
-                e.attachEvent ? e.attachEvent("onload", a) : e.addEventListener("load", a, !1)
-            }(window, document, window.Beacon || function() {});
-        </script>
-        <script type="text/javascript">
-            window.Beacon('init', 'a622d317-9f86-462f-9ad2-c13970d05d1f')
-        </script>
     </div><?php include 'footer.php'; ?>
 </body>
 
