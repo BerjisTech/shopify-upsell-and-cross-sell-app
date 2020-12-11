21696405dd012dcc2b11fc67575eb73c8cf52996
Berjis <berjistechnologies@gmail.com>
2020-12-11 10:50:55 +0300

    Frontend

--- application/views/home.php
+++ application/views/home.php
@@ -88,25 +88,6 @@
         <div class="bg-circle"></div>
         <div class="bg-circle-two"></div>
     </header>
-    <div class="section" id="intro">
-        <div class="container">
-            <div class="client-logos text-center">
-                <p class="text-muted">TRUSTED BY MOST POPULAR BRANDS</p>
-                <img src="fe/images/client_logo_1.png" alt="client logo" />
-                <img src="fe/images/client_logo_2.png" alt="client logo" />
-                <img src="fe/images/client_logo_3.png" alt="client logo" />
-                <img src="fe/images/client_logo_4.png" alt="client logo" />
-                <img src="fe/images/client_logo_5.png" alt="client logo" />
-                <img src="fe/images/client_logo_6.png" alt="client logo" />
-                <img src="fe/images/client_logo_7.png" alt="client logo" />
-                <img src="fe/images/client_logo_8.png" alt="client logo" />
-                <img src="fe/images/client_logo_9.png" alt="client logo" />
-                <img src="fe/images/client_logo_10.png" alt="client logo" />
-            </div>
-
-        </div>
-
-    </div>
 
     <div class="section bg-light pt-lg">
         <div class="container">
