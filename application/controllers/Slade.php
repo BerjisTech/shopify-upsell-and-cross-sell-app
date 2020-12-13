d649ec7e6fadcdf0671424839de223d6297f03b3
Berjis <berjistechnologies@gmail.com>
2020-12-13 08:08:33 +0300

    Frontend

--- application/controllers/Slade.php
+++ application/controllers/Slade.php
@@ -634,8 +634,8 @@ class Slade extends CI_Controller
 
                         $html .= '<div class="col-xs-12" style="padding-left: 0px !important; padding-right: 0px !important; margin-top: 10px; padding-bottom: 5px; border-bottom: 1px solid #C0C0C0;">';
                         $html .= '<div class="col-xs-12"><span class="pull-left" style="font-weight: bold; font-size: 18px; color: #333333;">' . $value['title'] . '</span> <span class="pull-right btn btn-primary btn-sm btn-icon icon-right" onclick="products[$(\'.toplect\').val()][\'rp\']=' . $value['id'] . ';products[$(\'.toplect\').val()][\'rp\'] = \'\';"><i style="color: #fff;" class="entypo-plus"></i> Replace product</span></div>';
-                        $html .= '<div class="col-xs-2" style="vertical-align: middle;"></div>';
-                        $html .= '<div class="col-xs-10" style="vertical-align: middle;">';
+                        $html .= '<div class="col-xs-1" style="vertical-align: middle;"></div>';
+                        $html .= '<div class="col-xs-11" style="vertical-align: middle;">';
 
                         foreach ($value['variants'] as $variant) {
                             $html .= '
