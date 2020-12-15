39e6ca5ad387d09cd8cc302ffbb3cfd4367f88bf
Berjis <berjistechnologies@gmail.com>
2020-12-15 17:50:54 +0300

    Frontend

--- application/controllers/Slade.php
+++ application/controllers/Slade.php
@@ -281,7 +281,7 @@ class Slade extends CI_Controller
             $shop = str_replace("/", "", $shop);
         endif;
         $api_key = $this->config->item('shopify_api_key');
-        $scopes = "read_orders,write_orders,read_draft_orders,read_products,read_product_listings,read_inventory,write_inventory,read_script_tags,write_script_tags,read_themes,write_themes,read_checkouts,write_checkouts,read_price_rules,write_price_rules,read_discounts,write_discounts,";
+        $scopes = "read_orders,read_draft_orders,read_products,read_product_listings,read_inventory,read_script_tags,write_script_tags,read_themes,read_checkouts,read_price_rules,read_discounts";
         $redirect_uri = base_url() . "generate_token";
 
         // Build install/approval URL to redirect to
