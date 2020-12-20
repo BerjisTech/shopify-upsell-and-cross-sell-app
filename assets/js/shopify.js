3cf3904984ba294851eb8eda302af8ab10147572
Berjis <berjistechnologies@gmail.com>
2020-12-20 16:15:41 +0300

    Shopify Plain

--- assets/js/shopify.js
+++ assets/js/shopify.js
@@ -45,13 +45,8 @@ function createSUW() {
         if (tgtRect.bottom > pRect.bottom) target.style.top = pRect.height - tgtRect.height + 'px';
     });
 
-    if (typeof jQuery === 'undefined' || jQuery == null) {
-        document.querySelector('body').insertAdjacentHTML('afterbegin', '<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>');
-        let j = jQuery.noConflict();
-        j('.suw_body').load('https://sleek-upsell.herokuapp.com/suw/' + Shopify.shop);
-    } else {
-        jQuery('.suw_body').load('https://sleek-upsell.herokuapp.com/suw/' + Shopify.shop);
-    }
+    document.querySelector('body').insertAdjacentHTML('afterbegin', '<script src="https://sleek-upsell.com/assets/js/jquery-1.11.3.min.js"></script>');
+    $('.suw_body').load('https://sleek-upsell.herokuapp.com/suw/' + Shopify.shop);
 
 
 
