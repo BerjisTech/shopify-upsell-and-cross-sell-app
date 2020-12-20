82749328bac30a7bb6d3e5498bcbc28389c8f186
Berjis <berjistechnologies@gmail.com>
2020-12-20 13:49:06 +0300

    Frontend

--- application/views/suw.php
+++ application/views/suw.php
@@ -1,8 +1,11 @@
 <script>
     var base_url = '<?php echo base_url(); ?>';
 </script>
-
+<script src="<?php echo base_url(); ?>assets/js/jquery-1.11.3.min.js"></script>
+<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/bootstrap.css" id="style-resource-4">
+<link rel="stylesheet" href="<?php echo base_url(); ?>assets/js/jquery-ui/css/no-theme/jquery-ui-1.10.3.custom.min.css" id="style-resource-1">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@icon/entypo@1.0.0-alpha.3/entypo.min.css" id="style-resource-2">
+<script src="<?php echo base_url(); ?>assets/js/bootstrap.js" id="script-resource-3"></script>
 
 
 
@@ -535,7 +538,7 @@
         </div>
     </div>
 </div>
-
+<script src="<?php echo base_url(); ?>assets/js/bootstrap-colorpicker.min.js" id="script-resource-14"></script>
 <script>
     $("#previewFrame").contents().find("div").each(function() {
         $(this).attr('onclick', 'console.log($(this).html())')
