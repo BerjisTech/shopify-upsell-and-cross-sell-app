aa454ff2585604ad56ae72359ccadd63a718248f
Berjis <berjistechnologies@gmail.com>
2021-02-06 04:21:34 +0300

    Don't show sold out products

--- application/controllers/Slade.php
+++ application/controllers/Slade.php
@@ -6,6 +6,7 @@ class Slade extends CI_Controller
     public function __construct()
     {
         parent::__construct();
+        
         /* cache control */
         $this->output->set_header('Last-Modified: ' . gmdate("D, d M Y H:i:s") . ' GMT');
         $this->output->set_header('Cache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-check=0');
