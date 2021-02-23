99aa19a5c09e575c45afe282b3c9b2b7f49cb31e
Berjis <berjistechnologies@gmail.com>
2021-02-23 23:23:51 +0300

    DigitalOcena

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://sleekupsell.com/';
+$config['base_url'] = 'https://sleek-upsell.com/';
 
 /*
 |--------------------------------------------------------------------------
